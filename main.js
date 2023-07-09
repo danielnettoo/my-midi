31395285f0d4e55aa50c50f9798b4fd0721487ff
Daniel Netto <45443380+danielnettoo@users.noreply.github.com>
2023-07-08 21:03:14 -0300

    commit 5
    - main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+function tocaPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function tocaPuf() {
+    document.querySelector('#som_tecla_puf').play();
+}
+
+function tocaSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function tocaToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function tocaPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tocaTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tocaTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+listaTeclas[0].onclick = tocaPom;
+
+
